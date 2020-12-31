49403fb3cebe8730b92fb252b06998311f3611c1
Vinay Sharma <svinay0488@gmail.com>
2020-12-31 18:25:28 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -7,29 +7,12 @@ function App() {
   
   return (
     
-    <BrowserRouter >
+    <BrowserRouter>
       <div>
-        {/* <Login /> */}
 
         <Navbar_ />
         
-        {/* <link
-          rel="stylesheet"
-          href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
-          integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh"
-          crossorigin="anonymous"
-        /><script src="https://unpkg.com/react/umd/react.production.min.js" crossorigin />
-
-        <script
-          src="https://unpkg.com/react-dom/umd/react-dom.production.min.js"
-          crossorigin
-        />
-
-        <script
-          src="https://unpkg.com/react-bootstrap@next/dist/react-bootstrap.min.js"
-          crossorigin
-        /> */}
-        <Router />
+        <Router basename="vinswalker/components" />
 
       </div>
     </BrowserRouter>
