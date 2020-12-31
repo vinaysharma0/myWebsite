ccf5c4e20ca37810ad6f5e76f46450b5eadbde76
Vinay Sharma <svinay0488@gmail.com>
2020-12-31 17:08:37 +0530

    Updates

---
+++ precache-manifest.ba0b0dadeac24cde0e78eab2cfff61c4.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33f7165a42ddfb70db66bc424a1dd85d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7389803b1a463d43f868",
+    "url": "/static/css/main.42e08a9c.chunk.css"
+  },
+  {
+    "revision": "d120d355dfe9bdab4ff5",
+    "url": "/static/js/2.dea686dd.chunk.js"
+  },
+  {
+    "revision": "caacfaae96ba8b2d6a270a80b7b8b4cb",
+    "url": "/static/js/2.dea686dd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7389803b1a463d43f868",
+    "url": "/static/js/main.f3e9fd39.chunk.js"
+  },
+  {
+    "revision": "79c19766b9fafc23cac3",
+    "url": "/static/js/runtime-main.e387f811.js"
+  },
+  {
+    "revision": "78aa4cb7842de83f0b96e93a347cec7b",
+    "url": "/static/media/Churn.78aa4cb7.png"
+  },
+  {
+    "revision": "cf9fa48d74abb7b5409d86c39f9a0f6b",
+    "url": "/static/media/DeepL.cf9fa48d.png"
+  },
+  {
+    "revision": "f9017eb8ba4eb6ad49819666e4f013e2",
+    "url": "/static/media/GoogleStock.f9017eb8.jpg"
+  },
+  {
+    "revision": "5caeff7c479166474f054ccca77b9d10",
+    "url": "/static/media/Machine.5caeff7c.png"
+  },
+  {
+    "revision": "bfe13fcd83401deb052414df324138fb",
+    "url": "/static/media/Netflix.bfe13fcd.png"
+  },
+  {
+    "revision": "8f2156115f7fb6e01a0b100c38f04e32",
+    "url": "/static/media/Position.8f215611.png"
+  },
+  {
+    "revision": "58eff72174ceda125476020ac99621a1",
+    "url": "/static/media/Resume.58eff721.pdf"
+  },
+  {
+    "revision": "ff7ba14dcb24b8a4bf9a8df9f08b3bc4",
+    "url": "/static/media/SocialNetwork.ff7ba14d.png"
+  },
+  {
+    "revision": "a0d78628daa48c9ca6c9c3df52aee8ef",
+    "url": "/static/media/all.a0d78628.jpg"
+  },
+  {
+    "revision": "b848d5d9bb221592064de0f356f61676",
+    "url": "/static/media/css.b848d5d9.png"
+  },
+  {
+    "revision": "fe87bdac525553e21512e3a3a5337cf0",
+    "url": "/static/media/gis.fe87bdac.png"
+  },
+  {
+    "revision": "0918d78648457def080137b179fc5608",
+    "url": "/static/media/github.0918d786.png"
+  },
+  {
+    "revision": "38a5b0148bb84905cca4cc83d7ba9317",
+    "url": "/static/media/gtc.38a5b014.png"
+  },
+  {
+    "revision": "25b694ba78065655dcee6237019aceda",
+    "url": "/static/media/html-5.25b694ba.png"
+  },
+  {
+    "revision": "8cf8eb388ca8bd792022b07bf9ce3766",
+    "url": "/static/media/main.8cf8eb38.jpg"
+  },
+  {
+    "revision": "00f5ec64a372c055cedcebb9acfad076",
+    "url": "/static/media/vinay_about.00f5ec64.jpg"
+  }
+]);
